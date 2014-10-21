2f88fd3ad603a801ddaae356a622558d926c796f
Poe <yxluozengbo@gmail.com>
2014-10-21 20:35:08 +0800

    Climbing Stairs

---
+++ Climbing Stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int a = 1;
+        int b = 2;
+        if(n == 1)
+            return a;
+        if(n == 2)
+            return b;
+        for(int i = 2;i < n;i++){
+            int tmp = a;
+            a = b;
+            b = tmp + b;
+        }
+        return b;
+    }
+};
