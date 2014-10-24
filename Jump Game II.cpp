787aaa00884f4ba03b5aa4e4b92a577ad07a174a
Poe <yxluozengbo@gmail.com>
2014-10-24 16:50:28 +0800

    Jump Game II

---
+++ Jump Game II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int jump(int A[], int n) {
+        int jump = 0;
+        int last = 0;//the last max range
+        int cur = 0;//the current max range
+        for(int i = 0; i < n;i++){
+            if(last < i){//need a jump
+                jump++;
+                last = cur;
+            }
+            cur = max(cur,A[i] + i);
+        }
+        return jump;
+    }
+};
