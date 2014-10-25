ee2fc42ee4985e53f59b40b1c94753498bf1a8ef
Poe <yxluozengbo@gmail.com>
2014-10-25 15:21:15 +0800

    solution only need constant memory space

--- Populating Next Right Pointers in Each Node.cpp
+++ Populating Next Right Pointers in Each Node.cpp
@@ -32,3 +32,32 @@ public:
         }
     }
 };
+
+
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if (!root || !root->left)  return;
+        
+        auto parent = root, son = root->left;
+        
+        while (son)
+        {
+            auto back = son;
+            
+            while (son)
+            {
+                son->next = parent->right;
+                son = son->next;
+                parent = parent->next;
+                son->next = parent?parent->left:nullptr;
+                son = son->next;
+            }
+            
+            parent = back;
+            son = parent->left;
+        }
+    }
+};
