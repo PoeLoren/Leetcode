5c2bcb0c7c03100d691c399b9e0f0aa2bc5e6dd6
Poe <yxluozengbo@gmail.com>
2014-11-13 15:13:24 +0800

    Gas Station

---
+++ Gas Station.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+        if(gas.size() == 0 || cost.size() == 0 ||gas.size() != cost.size())
+            return -1;
+        int total = 0,sum = 0,start = 0;
+        for(int i = 0; i < gas.size();i++){
+            total += gas[i]-cost[i];
+            if(sum < 0){
+                sum = gas[i] - cost[i];
+                start = i;
+            }else{
+                sum += gas[i] - cost[i];
+            }
+        }
+        return total < 0 ?-1:start;
+    }
+};
