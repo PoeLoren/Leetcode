f2d8586375af6b57cfc049f7a4382412d13c3ef5
Poe <yxluozengbo@gmail.com>
2014-11-30 15:56:06 +0800

    Minimum Window Substring

---
+++ Minimum Window Substring.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string minWindow(string S, string T) {
+        const int ASCII = 256;
+        int exp[ASCII] = {0};
+        int app[ASCII] = {0};
+        for(int i = 0;i < T.size();i++)
+            exp[T[i]]++;
+        int minSize = INT_MAX;
+        int minStart = 0;
+        int winStart = 0;
+        int appear = 0;
+        for(int i = 0;i < S.size();i++)
+        {
+            if(exp[S[i]] > 0)
+            {
+                app[S[i]]++;
+                if(app[S[i]] <= exp[S[i]])
+                    appear++;
+            }
+            if(appear == T.size())
+            {
+                while(app[S[winStart]] > exp[S[winStart]]   //if the number of start char is greater than expected, it can be pass
+                    ||exp[S[winStart]] == 0)                //or start char is not in the T string
+                {
+                    app[S[winStart]]--;
+                    winStart++;
+                }
+                if(minSize > (i - winStart + 1))
+                {
+                    minSize = i - winStart + 1;
+                    minStart = winStart;
+                }
+            }
+            
+        }
+        if(minSize == INT_MAX)
+            return "";
+        else
+            return S.substr(minStart,minSize);
+            
+    }
+};
