c61945b47ca1e23d45bce16ec82513ba7bf8e347
Poe <yxluozengbo@gmail.com>
2014-12-02 20:34:41 +0800

    Rotate Image

---
+++ Rotate Image.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void rotate(vector<vector<int> > &matrix) {
+        int n = matrix.size();
+        for(int i = 0;i < n - 1;i++)
+            for(int j = 0;j < n - i - 1;j++)
+            {
+                int x = n - j - 1;
+                int y = n - i - 1 ;
+                swap(matrix[i][j],matrix[x][y]);
+            }
+        for(int i = 0;i < n / 2;i++)
+        {
+            for(int j = 0;j < n;j++)
+            {
+                int x = n - i - 1;
+                swap(matrix[i][j],matrix[x][j]);
+            }
+        }
+    }
+};
