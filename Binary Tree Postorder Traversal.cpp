d77548bdf86acbbc398c1a5b268454c3e1f42f0f
Poe <yxluozengbo@gmail.com>
2014-12-14 15:45:56 +0800

    Binary Tree Postorder Traversal

--- Binary Tree Postorder Traversal.cpp
+++ Binary Tree Postorder Traversal.cpp
@@ -37,3 +37,26 @@ public:
         
     }
 };
+
+class Solution {
+public:
+   vector<int> postorderTraversal(TreeNode *root)   
+    {  
+        vector<int> rs;  
+        if (!root) return rs;  //若为空树，则返回空vector
+        stack<TreeNode *> stk;  
+        stk.push(root);  //当前节点入栈
+        while (!stk.empty())  
+        {  
+            TreeNode *t = stk.top();  //栈顶节点出栈、输出
+            stk.pop();  
+            rs.push_back(t->val);  
+            //注意，下面入栈顺序不能错 ，因为先左后右，
+            //这样出栈时先遍历才是右（中->右->左）
+            if (t->left) stk.push(t->left);  
+            if (t->right) stk.push(t->right);  
+        }  
+        reverse(rs.begin(), rs.end());  //逆序，就成了后序遍历了
+        return rs;  
+    }  
+};
