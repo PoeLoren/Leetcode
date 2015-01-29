e7f0aacb810141e72c462dc72774e43cdedece9f
Poe <yxluozengbo@gmail.com>
2015-01-29 16:04:14 +0800

    modified divide two integers.cpp for cornor cases

--- Divide Two Integers.cpp
+++ Divide Two Integers.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
         bool flag = true;
-        if(dividend < 0&&divisor > 0 || dividend >0&&divisor < 0){
+        if((dividend < 0&&divisor > 0) || (dividend >0&&divisor < 0)){
             flag = false;
         }
         long long c = dividend;//abs(-2147483648) will overflow
@@ -11,9 +11,9 @@ public:
         long long b = abs(d);
         if(a < b)
             return 0;
-        int res = 0;
+        long long res = 0;
         while(a >= b){
-            int count = 1;
+            long long count = 1;
             long long sum = b;
             while(a >= sum + sum){
                 sum += sum;
@@ -21,10 +21,11 @@ public:
             }
             res += count;
             a -= sum;
-            
         }
         if(!flag)
             res = -res;
+        else if(res > INT_MAX) //res = 2147483648 will overflow
+            res = INT_MAX;
         return res;
     }
 };
