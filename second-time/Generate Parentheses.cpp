6a8bb0f3ef82c8d7664ebf3103f2e97a66d596d7
Poe <yxluozengbo@gmail.com>
2015-03-07 20:35:02 +0800

    Generate Parentheses

---
+++ second-time/Generate Parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> res;
+    vector<string> generateParenthesis(int n) {
+        string s;
+        genParen(0, 0, s, n);
+        return res;
+    }
+    
+    void genParen(int l, int r, string& s, int n){
+        if(l == r && l + r == 2 * n)
+            res.push_back(s);
+        else if(l < r || l > n || r > n)
+            return;
+        else{
+            s.push_back('(');
+            genParen(l + 1, r, s, n);
+            s.pop_back();
+            s.push_back(')');
+            genParen(l, r + 1, s, n);
+            s.pop_back();
+        }
+    }
+};
