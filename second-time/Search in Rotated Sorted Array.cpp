a163c44da36043d75b5b453af9eebe5f1007a7c1
Poe <yxluozengbo@gmail.com>
2015-04-12 20:51:00 +0800

    Search in Rotated Sorted Array-changed

--- second-time/Search in Rotated Sorted Array.cpp
+++ second-time/Search in Rotated Sorted Array.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     int search(int A[], int n, int target) {
         int l = 0;
-        int r = n;
-        while(l < r)
+        int r = n - 1;
+        while(l <= r)
         {
             int mid = (l + r) >> 1;
             if(A[mid] == target)
                 return mid;
-            if(A[l] < A[mid])
+            if(A[l] <= A[mid])
             {
                 if(A[l] <= target && target < A[mid])
-                    r = mid;
+                    r = mid - 1;
                 else
                     l = mid + 1;
             }
             else
             {
-                if(A[mid] < target && target <= A[r - 1])
+                if(A[mid] < target && target <= A[r])
                     l = mid + 1;
                 else
-                    r = mid;
+                    r = mid - 1;
             }
         }
         return -1;
