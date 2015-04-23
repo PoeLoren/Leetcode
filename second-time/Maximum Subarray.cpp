6d7ca04a71cb040183c7dce5d33c205ec3285f93
Poe <yxluozengbo@gmail.com>
2015-04-23 15:28:28 +0800

    Maximum Subarray

---
+++ second-time/Maximum Subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = nums[0];
+        int cur = 0;
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            cur = max(nums[i], cur + nums[i]);
+            sum = max(sum, cur);
+        }
+        return sum;
+    }
+};
