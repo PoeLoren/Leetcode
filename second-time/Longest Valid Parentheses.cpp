686a926fa36c826b3db5c2d79f35b6faac7f9840
PoeLoren <yxluozengbo@gmail.com>
2015-11-02 15:00:11 +0800

    another solution

--- second-time/Longest Valid Parentheses.cpp
+++ second-time/Longest Valid Parentheses.cpp
@@ -1,6 +1,40 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
+        stack<int> idxStk;
+        stack<int> lstk;
+        idxStk.push(-1);
+        int res = 0;
+        for(int i = 0; i < s.size(); ++i)
+        {
+            if(s[i] == '(')
+            {
+                idxStk.push(i);
+                lstk.push(i);
+            }
+            else
+            {
+                if(lstk.empty())
+                {
+                    idxStk.push(i);
+                }
+                else
+                {
+                    idxStk.pop();
+                    lstk.pop();
+                    int pre = idxStk.top();
+                    res = max(res, i - pre);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
         stack<int> idx;
         int res = 0;
         int last = -1;
